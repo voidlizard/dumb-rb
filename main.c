0c95ee388bd230f2ab242d98c590e2eb23e94ca3
Dmitry Zuikov <dzuikov@gmail.com>
2012-03-10 12:01:43 +0400

    ringbuf init test passed

--- main.c
+++ main.c
@@ -13,6 +13,8 @@ typedef struct ring_buffer_t_ {
     uint8_t data[1];
 } ringbuffer_t;
 
+#define ringbuffer_shift_ptr(p, s, e, w) ((p) + (w) <= (e) ? (p) + (w) : (s) + ((w) - ((e)-(p))))
+
 void ringbuffer_reset(ringbuffer_t *rb) {
     rb->bs = &rb->data[0];
     rb->be = &rb->data[rb->data_size];
@@ -33,41 +35,32 @@ ringbuffer_t* ringbuffer_alloc(size_t data_size, uint8_t *data) {
     return (ringbuffer_t*)0;
 }
 
-size_t ringbuffer_read_avail(ringbuffer_t *rb) {
-    
+size_t ringbuffer_write_avail(ringbuffer_t *rb) {
     uint8_t *rp = rb->rp;
     uint8_t *wp = rb->wp;
     uint8_t *bs = rb->bs;
     uint8_t *be = rb->be;
     size_t  written = rb->written;
 
-    if( rp < wp ) { // LESS
-        return (size_t)(wp - rp);
-    } else if( rp > wp  ) { // MORE
-        return ((size_t)(be - rp) + (size_t)(wp - bs));
+    if( wp < rp ) {
+        return (size_t)(rp - wp);
     }
 
-    return 0;
-}
-
-size_t ringbuffer_write_avail(ringbuffer_t *rb) {
-    
-    uint8_t *rp = rb->rp;
-    uint8_t *wp = rb->wp;
-    uint8_t *bs = rb->bs;
-    uint8_t *be = rb->be;
-    size_t  written = rb->written;
-
-    if( wp < rp ) { // LESS
-        return (size_t)(rp - wp);
-    } else if( rp > wp  ) { // MORE
+    if( wp > rp ) {
         return ((size_t)(be - wp) + (size_t)(rp - bs));
     }
 
+    if( wp == rp && written == 0 ) {
+        return ((size_t)(be - wp) + (size_t)(wp - bs));
+    }
+
+    if( wp == rp && written != 0 ) {
+        return 0;
+    }
+
     return 0;
 }
 
-#define ringbuffer_shift_ptr(p, s, e, w) ((p) + (w) <= (e) ? (p) + (w) : (s) + ((w) - ((e)-(p))))
 
 size_t ringbuffer_write(ringbuffer_t *rb, void *src, size_t size) {
     size_t towrite = size;
@@ -100,27 +93,55 @@ size_t ringbuffer_write(ringbuffer_t *rb, void *src, size_t size) {
 
 }
 
+size_t ringbuffer_read_avail(ringbuffer_t *rb) {
+    uint8_t *rp = rb->rp;
+    uint8_t *wp = rb->wp;
+    uint8_t *bs = rb->bs;
+    uint8_t *be = rb->be;
+    size_t  written = rb->written;
+
+    if( rp > wp ) {
+        return ((size_t)(be - rp) + (size_t)(wp - bs));
+    }
+
+    if( rp < wp ) {
+        return ((size_t)(wp - rp));
+    }
+
+    if( wp == rp && written == 0 ) {
+        return 0;
+    }
+
+    if( wp == rp && written != 0 ) {
+        return ((size_t)(be - rp) + (size_t)(bs - wp));
+    }
+
+    return 0;
+}
+
+
 size_t ringbuffer_read(ringbuffer_t *rb, void *dst, size_t size) {
 }
 
-uint8_t databuf[(sizeof(ringbuffer_t) - 1 + 256)];
+
+int test_case_1() {
+    ringbuffer_t *rb;
+    static uint8_t databuf[(sizeof(ringbuffer_t) - 1 + 256)];
+    printf("TEST CASE #1 :: Buffer init\n");
+    rb = ringbuffer_alloc(sizeof(databuf), databuf);
+    printf("TEST CASE #1 :: LOG :: %d %d %d\n", rb->data_size, ringbuffer_read_avail(rb), ringbuffer_write_avail(rb));
+    if( rb->data_size == 256 && ringbuffer_write_avail(rb) == 256 && ringbuffer_read_avail(rb) == 0 ) {
+        printf("TEST CASE #1 :: RESULT = PASS\n");
+        return 0;
+    }
+    printf("TEST CASE #1 :: RESULT = FAIL\n");
+    return (-1);
+}
+
 
 int main(void) {
-    ringbuffer_t *rb;
 
-    printf("%d %d\n", sizeof(ringbuffer_t), sizeof(databuf));
-    rb = ringbuffer_alloc(sizeof(databuf), databuf);
-    printf("RB (p: %08X) (size: %u)\n", rb, rb->data_size);
-
-    // TEST
-    printf("test1 --- (wa: %d) (ra: %d)\n", ringbuffer_write_avail(rb), ringbuffer_read_avail(rb));
-
-    // TEST  ---
-/*    while( ringbuffer_write_avail(rb) ) {*/
-/*        uint8_t pattern[3] = { 0xFA, 0xFE, 0xAA };*/
-/*        ringbuffer_write(rb, pattern, 3);*/
-/*        printf("(wa: %d) (ra: %d)\n", ringbuffer_write_avail(rb), ringbuffer_read_avail(rb));*/
-/*    }*/
+    test_case_1();
 
     return 0;
 }

6296dca9384dfc69c35547e0668f95920b483628
Dmitry Zuikov <dzuikov@gmail.com>
2012-03-10 11:45:27 +0400

    fuckup

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ typedef struct ring_buffer_t_ {
     uint8_t *be;
     uint8_t *rp;
     uint8_t *wp;
+    size_t  written;
     size_t  data_size;
     uint8_t data[1];
 } ringbuffer_t;
@@ -17,6 +18,7 @@ void ringbuffer_reset(ringbuffer_t *rb) {
     rb->be = &rb->data[rb->data_size];
     rb->rp = rb->bs;
     rb->wp = rb->bs;
+    rb->written = 0;
 }
 
 ringbuffer_t* ringbuffer_alloc(size_t data_size, uint8_t *data) {
@@ -37,6 +39,7 @@ size_t ringbuffer_read_avail(ringbuffer_t *rb) {
     uint8_t *wp = rb->wp;
     uint8_t *bs = rb->bs;
     uint8_t *be = rb->be;
+    size_t  written = rb->written;
 
     if( rp < wp ) { // LESS
         return (size_t)(wp - rp);
@@ -53,6 +56,7 @@ size_t ringbuffer_write_avail(ringbuffer_t *rb) {
     uint8_t *wp = rb->wp;
     uint8_t *bs = rb->bs;
     uint8_t *be = rb->be;
+    size_t  written = rb->written;
 
     if( wp < rp ) { // LESS
         return (size_t)(rp - wp);
@@ -77,25 +81,19 @@ size_t ringbuffer_write(ringbuffer_t *rb, void *src, size_t size) {
         uint8_t *wp = rb->wp;
         uint8_t *bs = rb->bs;
         uint8_t *be = rb->be;
+/*        size_t chunk1 = (size_t)(wp > rp ? (be - wp) : (rp - wp));*/
+/*        size_t chunk2 = (size_t)((wp > rp) ? towrite - chunk1 : 0);*/
+/*        size_t w1     = towrite <= chunk1 ? towrite : chunk1;*/
+/*        size_t w2     = */
+/*        */
+/*        if( chunk ) memcpy(wp, src, w1);*/
+/*        wp = ringbuffer_shift_ptr(wp, bs, be, chunk);*/
+/*        if( rest )  memcpy(wp, src, rest);*/
+/*        wp = ringbuffer_shift_ptr(wp, bs, be, chunk);*/
 
-        if( wp > rp ) {
-            size_t chunk = (size_t)(be - wp);
-            if( towrite <= chunk ) {
-                memcpy(wp, src, towrite);
-            } else {
-                memcpy(wp, src, chunk);
-                memcpy(bs, src + chunk, towrite - chunk);
-            }
-            wp = ringbuffer_shift_ptr(wp, bs, be, towrite);
-        } else {
-            size_t chunk = (size_t)(rp - wp);
-            if( towrite <= chunk ) {
-                memcpy(wp, src, towrite);
-            } else {
-                memcpy(wp, src, chunk);
-            }
-            wp = ringbuffer_shift_ptr(wp, bs, be, towrite);
-        }
+/*        rb->wp = wp;*/
+/*        rb->written += towrite;*/
+
     }
 
     return towrite;
@@ -113,6 +111,16 @@ int main(void) {
     printf("%d %d\n", sizeof(ringbuffer_t), sizeof(databuf));
     rb = ringbuffer_alloc(sizeof(databuf), databuf);
     printf("RB (p: %08X) (size: %u)\n", rb, rb->data_size);
+
+    // TEST
+    printf("test1 --- (wa: %d) (ra: %d)\n", ringbuffer_write_avail(rb), ringbuffer_read_avail(rb));
+
+    // TEST  ---
+/*    while( ringbuffer_write_avail(rb) ) {*/
+/*        uint8_t pattern[3] = { 0xFA, 0xFE, 0xAA };*/
+/*        ringbuffer_write(rb, pattern, 3);*/
+/*        printf("(wa: %d) (ra: %d)\n", ringbuffer_write_avail(rb), ringbuffer_read_avail(rb));*/
+/*    }*/
 
     return 0;
 }
